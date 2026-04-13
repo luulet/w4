70436a75d4a095934ef13c71d9865e003c644173
luulee <luulee@github.com>
2026-04-13 15:01:37 +0300

    lab assignment 1 done

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,45 +1,27 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import MediaRow from '../components/MediaRow.jsx';
 import SingleView from '../components/SingleView.jsx';
-const mediaArray = [
-  {
-    media_id: 8,
-    user_id: 5,
-    filename: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    thumbnail: 'https://placecats.com/125/100',
-    filesize: 170469,
-    media_type: 'image/jpeg',
-    title: 'Picture 1',
-    description: 'This is a placeholder picture.',
-    created_at: '2026-01-07T20:49:34.000Z',
-  },
-  {
-    media_id: 9,
-    user_id: 7,
-    filename: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    thumbnail: 'https://placecats.com/125/102',
-    filesize: 1002912,
-    media_type: 'image/jpeg',
-    title: 'Pic 2',
-    description: '',
-    created_at: '2024-01-07T21:32:27.000Z',
-  },
-  {
-    media_id: 17,
-    user_id: 2,
-    filename:
-        'https://images.unsplash.com/photo-1513245543132-31f507417b26?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    thumbnail: 'https://images.unsplash.com/photo-1551272744-19456affaa89?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    filesize: 1236616,
-    media_type: 'video/mp4',
-    title: 'Bunny',
-    description: 'Butterflies fly around the bunny.',
-    created_at: '2024-01-07T20:48:13.000Z',
-  },
-];
+import {fetchData} from "../utils/fetch-data.js";
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const getMedia = async () => {
+    try {
+      const json = await fetchData('test.json');
+      setMediaArray(json);
+    } catch (error) {
+      console.error('Error fetching media', error);
+    }
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
+  // console.log(mediaArray);
 
   return (
       <>
